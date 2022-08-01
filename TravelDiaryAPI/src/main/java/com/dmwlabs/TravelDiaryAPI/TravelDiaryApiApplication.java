e42f1fa32055e7ef042af39630488ab50bb53de8
Dsoftwaredev <danielsoftwaredev@outlook.com>
2022-08-01 15:06:16 +0100

    setup

---
+++ TravelDiaryAPI/src/main/java/com/dmwlabs/TravelDiaryAPI/TravelDiaryApiApplication.java
@@ -0,0 +1,13 @@
+package com.dmwlabs.TravelDiaryAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelDiaryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelDiaryApiApplication.class, args);
+	}
+
+}
---
+++ TravelDiaryAPI/src/test/java/com/dmwlabs/TravelDiaryAPI/TravelDiaryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmwlabs.TravelDiaryAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelDiaryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
